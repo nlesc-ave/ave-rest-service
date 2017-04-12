ac5d9406ec0380ef1577726876d02ae7d1464f99
Mateusz Kuzak <mateusz.kuzak@gmail.com>
2017-04-12 10:22:32 +0200

    Make avedata a python package

---
+++ avedata/__init__.py
@@ -0,0 +1,1 @@
+from .avedata import server
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='avedata',
+    packages=['avedata'],
+    include_package_data=True,
+    install_requires=[
+        'connexion',
+    ],
+)
