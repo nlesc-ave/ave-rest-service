53b79891d7af15292e3b49b11afa977c7014ba48
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2017-09-27 12:24:05 +0200

    Dont store copies of variant info, use variant indexes in haplotype

--- avedata/variants.py
+++ avedata/variants.py
@@ -90,10 +90,13 @@ def get_variants(variant_file, chrom_id, start_position, end_position, accession
     # positions of the variants in a dictionary
     # fetch the genotypes in the variation positions
     # store all the variant objects in an array
-    variants = []
+    variants = {}
+    # [variant_index][accession] = {}
+    genotypes = {}
     for v in vcf_variants:
         if v.is_snp:
             an = alt2ambiguousnucleotide(v.ALT)
+            variant_index = v.CHROM + '/' + str(v.POS) + '/' + an
             variant = {
                 'chrom': v.CHROM,
                 'pos': v.POS,
@@ -103,9 +106,10 @@ def get_variants(variant_file, chrom_id, start_position, end_position, accession
                 'alt_ambiguous_nucleotide': an,
                 'qual': v.QUAL,
                 'filter': v.FILTER,
-                'info': dict(v.INFO),
-                'genotypes': []
+                'info': dict(v.INFO)
             }
+            variants[variant_index] = variant
+            genotypes[variant_index] = {}
             for idx, (acc, genotype) in enumerate(zip(all_accessions, v.genotypes)):
                 if acc not in accessions:
                     continue
@@ -116,15 +120,16 @@ def get_variants(variant_file, chrom_id, start_position, end_position, accession
                     # add info to variant object
                     # genotype should contain all format fields for each
                     # actual variant at this position
-                    genotype = {
+                    genotype4acc = {
                         'accession': acc,
                         'genotype': str(genotype[:2]),
                     }
                     for f in v.FORMAT[1:]:
-                        genotype[f] = str(v.format(f)[idx])
-                    variant['genotypes'].append(genotype)
-            variants.append(variant)
-    return variants, sequences, accessions
+                        genotype4acc[f] = str(v.format(f)[idx])
+                    genotypes[variant_index][acc] = genotype4acc
+
+    vcf.close()
+    return variants, sequences, genotypes
 
 
 def cluster_sequences(sequences):
@@ -140,32 +145,35 @@ def cluster_sequences(sequences):
     return list(clusters.values())
 
 
-def add_variants2haplotypes(haplotypes, variants):
+def add_genotypes2haplotypes(haplotypes, genotypes):
     # add variant information to haplotypes
     # variants should only contain genotype information
     # about genotypes present in particular haplotype
     for haplotype in haplotypes:
         haplotype['variants'] = []
-        for v in variants:
-            genotypes = []
-            for g in v['genotypes']:
-                if g['accession'] in haplotype['accessions']:
-                    genotypes.append(g)
-            if len(genotypes):
-                haplotype_variant = deepcopy(v)
-                haplotype_variant['genotypes'] = genotypes
+        for (variant_index, genotypes_by_accession) in genotypes.items():
+            filtered_genotypes = []
+            for (accession, genotype) in genotypes_by_accession.items():
+                if accession in haplotype['accessions']:
+                    filtered_genotypes.append(genotype)
+            if filtered_genotypes:
+                haplotype_variant = {
+                    'genotypes': filtered_genotypes,
+                    'variant_index': variant_index
+                }
                 haplotype['variants'].append(haplotype_variant)
 
 
-def add_sequence2haplotypes(haplotypes, ref_seq, start_position):
+def add_sequence2haplotypes(haplotypes, variants, ref_seq, start_position):
     # reconstruct the sequence based on reference and variant information of the
     # haplotype; both the sequence (a python string) and the variants from vcf
     # are indexed from zero
     for h in haplotypes:
         haplotype_sequence = list(ref_seq)
-        for v in h['variants']:
+        for haplotype_variant in h['variants']:
+            variant = variants[haplotype_variant['variant_index']]
             # start_position is 1-based and vcf, while seq is 0-based, require -1
-            haplotype_sequence[v['pos'] - start_position - 1] = v['alt'][0]
+            haplotype_sequence[variant['pos'] - start_position - 1] = variant['alt_ambiguous_nucleotide']
         h['sequence'] = "".join(haplotype_sequence)
 
 
@@ -206,11 +214,11 @@ def cluster_haplotypes(haplotypes):
 
 
 def get_haplotypes(variant_file, ref_file, chrom_id, start_position, end_position, accessions):
-    (variants, sequences, accessions) = get_variants(variant_file, chrom_id, start_position, end_position,
-                                         accessions)
+    (variants, sequences, genotypes) = get_variants(variant_file, chrom_id, start_position, end_position, accessions)
+    accessions = sequences.keys()
 
     haplotypes = cluster_sequences(sequences)
-    add_variants2haplotypes(haplotypes, variants)
+    add_genotypes2haplotypes(haplotypes, genotypes)
 
     # load reference sequence from a 2bit file
     ref_seq = get_sequence(ref_file, chrom_id, start_position, end_position)
@@ -218,13 +226,14 @@ def get_haplotypes(variant_file, ref_file, chrom_id, start_position, end_positio
     if len(haplotypes) == 0:
         return no_variants_response(accessions, ref_seq)
 
-    add_sequence2haplotypes(haplotypes, ref_seq, start_position)
+    add_sequence2haplotypes(haplotypes, variants, ref_seq, start_position)
 
     (hierarchy, ordered_haplotypes) = cluster_haplotypes(haplotypes)
 
     return {
         'hierarchy': hierarchy,
-        'haplotypes': ordered_haplotypes
+        'haplotypes': ordered_haplotypes,
+        'variants': variants
     }
 
 
@@ -239,7 +248,8 @@ def no_variants_response(accessions, ref_seq):
         'hierarchy': {
             'haplotype_id': haplotype['haplotype_id']
         },
-        'haplotypes': [haplotype]
+        'haplotypes': [haplotype],
+        'variants': {}
     }
 
 
